7db334bcb7f1586a6ebdb61c859204c00f640891
yohshiy <yohshiy@gmail.com>
2016-02-14 18:33:13 +0900

    lodash 用のサンプル完了

--- lang/js/lodash/lodash_sample.js
+++ lang/js/lodash/lodash_sample.js
@@ -1,4 +1,6 @@
-﻿var _ = require('lodash');
+﻿// イントロダクション
+
+var _ = require('lodash');
 
 var src = "3,2,9,6".split(",");
 
@@ -15,35 +17,38 @@ console.log(prod);		  // 27
 var result;
 
 // 逐次処理
+console.log("\n==== forEach ====");
 _.forEach([3,2,9,6],
-		  function(elem) { console.log(elem);});
+	  function(elem) { console.log(elem);});
 
 _([3,2,9,6]).forEach(function(elem) { console.log(elem);});
 
 
 
 // 変換
+console.log("\n==== map ====");
 result = _.map([3,2,9,6],
-				   function(elem) {return elem*2;});
+	       function(elem) {return elem*2;});
 console.log(result);
 
 result = _.map([3,2,9,6],
-				   elem => elem*2 );
+	       elem => elem*2 );
 console.log(result);
 
 
 
 // フィルター
+console.log("\n==== filter ====");
 result = _.filter([3,2,9,6],
-					  elem => (elem % 2) == 1);
+		  elem => (elem % 2) == 1);
 console.log(result);
 
 
 
 // フィルター 非関数版
 var objs = [{'name': 'Tanaka', 'age': 35, 'enrolled': true},
-			{'name': 'Yamada', 'age': 30, 'enrolled': false},
-			{'name': 'Satou',  'age': 41, 'enrolled': true}];
+	    {'name': 'Yamada', 'age': 30, 'enrolled': false},
+	    {'name': 'Satou',  'age': 41, 'enrolled': true}];
 
 // matches 呼び出し
 result = _.filter(objs, {'age': 35, 'enrolled': true});
@@ -55,21 +60,29 @@ console.log(result);            // [ { name: 'Tanaka', age: 35, enrolled: true }
 
 result = _.filter(objs, 'enrolled');
 console.log(result);            // [ { name: 'Tanaka', age: 35, enrolled: true },
-                                //  { name: 'Satou', age: 41, enrolled: true } ]
+//  { name: 'Satou', age: 41, enrolled: true } ]
 
+
+// 畳み込み
+console.log("\n==== reduce ====");
+var src = [3,2,9,6]
+result = _.reduce(src, (sum, elem) => sum + elem);
+console.log(result);			// 20
+result = _.reduce(src, (max, elem) => (max < elem) ? elem : max);
+console.log(result);			// 9
+result = _.reduce(src, (count, elem) => count+1, 0);
+console.log(result);			// 4
 
 
 // ソート 
-
+console.log("\n==== sortBy ====");
 var objs = [{'name': 'Tanaka', 'age': 35},
-			{'name': 'Yamada', 'age': 30},
-			{'name': 'Suzuki', 'age': 28},
-			{'name': 'Satou',  'age': 41}];
+	    {'name': 'Yamada', 'age': 30},
+	    {'name': 'Satou',  'age': 41}];
 
 // age の値でソート
 result = _.sortBy(objs, 'age');
 console.log(result);
-// [ { name: 'Suzuki', age: 28 },
 //   { name: 'Yamada', age: 30 },
 //   { name: 'Tanaka', age: 35 },
 //   { name: 'Satou', age: 41 } ]
@@ -79,8 +92,7 @@ result = _.sortBy(objs, elem => elem.age * -1);
 console.log(result);
 // [ { name: 'Satou', age: 41 },
 //   { name: 'Tanaka', age: 35 },
-//   { name: 'Yamada', age: 30 },
-//   { name: 'Suzuki', age: 28 } ]
+//   { name: 'Yamada', age: 30 } ]
 
 
 // 関数で指定
@@ -89,27 +101,51 @@ result = _.sortBy(strs);
 console.log(result);			// [ 'BAZ', 'bar', 'foo', 'qux' ]
 
 result = _.sortBy(strs,
-				  elem => _.toLower(elem) );
+		  elem => _.toLower(elem) );
 console.log(result);			// [ 'bar', 'BAZ', 'foo', 'qux' ]
 
 
 var points = [{'x': 2, 'y': 8},
-			  {'x': 5, 'y': 1},
-			  {'x': 2, 'y': 4}];
+	      {'x': 5, 'y': 1},
+	      {'x': 2, 'y': 4}];
 
-// x でソートし、同じなら y でソート
-result = _.sortBy(points, 'x', 'y');
+// x でソートし、x が同じなら y でソート
+result = _.sortBy(points, ['x', 'y']);
 console.log(result);
 // [ { x: 2, y: 4 }, { x: 2, y: 8 }, { x: 5, y: 1 } ]
 
 
+// その他のメソッド
+console.log("\n==== Others ====");
 
-// 畳み込み
-var src = [3,2,9,6]
-result = _.reduce(src, (sum, elem) => sum + elem);
-console.log(result);			// 20
-result = _.reduce(src, (max, elem) => (max < elem) ? elem : max);
-console.log(result);			// 9
-result = _.reduce(src, (count, elem) => count+1, 0);
-console.log(result);			// 4
 
+result = _.size([3,2,9,6]);
+console.log(result);		// 4
+
+
+result = _.find([3,2,9,6], elem => (elem % 2) == 1);
+console.log(result);		// 3
+
+result = _.findLast([3,2,9,6], elem => (elem % 2) == 1);
+console.log(result);		// 9
+
+result = _.includes([3,2,9,6], 2);
+console.log(result);		// true
+
+result = _.some([3,2,9,6], elem => (elem % 2) == 1);
+console.log(result);		// true
+result = _.every([3,2,9,6], elem => (elem % 2) == 1);
+console.log(result);		// false
+
+
+
+// メソッド連結と遅延評価
+
+console.log("\n==== Lazy ====");
+
+result = 
+    _.chain([3,2,9,6])		// _([3,2,9,6]) と同じ
+    .filter(elem => (elem % 2) == 1)
+    .map(elem => elem * 2)
+    .value();
+console.log(result);		// [ 6, 18 ]
---
+++ lang/js/lodash/lodash_sample_short.js
@@ -0,0 +1,105 @@
+﻿
+// 逐次処理
+_.forEach([3,2,9,6],
+	  function(elem) { console.log(elem);});
+
+_([3,2,9,6]).forEach(function(elem) { console.log(elem);});
+
+
+// 変換
+_.map([3,2,9,6],
+      function(elem) {return elem*2;});
+// [ 6, 4, 18, 12 ]
+
+_.map([3,2,9,6],
+      elem => elem*2 );
+
+
+
+// フィルター
+_.filter([3,2,9,6], elem => (elem % 2) == 1);
+
+
+
+// フィルター 非関数版
+var objs = [{'name': 'Tanaka', 'age': 35, 'enrolled': true},
+	    {'name': 'Yamada', 'age': 30, 'enrolled': false},
+	    {'name': 'Satou',  'age': 41, 'enrolled': true}];
+
+// matches 呼び出し
+_.filter(objs, {'age': 35, 'enrolled': true});
+// [ { name: 'Tanaka', age: 35, enrolled: true } ]
+
+// matchesProperty 呼び出し
+_.filter(objs, ['age', 35]);
+// [ { name: 'Tanaka', age: 35, enrolled: true } ]
+
+_.filter(objs, 'enrolled');
+// [ { name: 'Tanaka', age: 35, enrolled: true },
+//   { name: 'Satou', age: 41, enrolled: true } ]
+
+
+// 畳み込み
+var src = [3,2,9,6]
+_.reduce(src, (sum, elem) => sum + elem);		 // 20
+_.reduce(src, (max, elem) => (max < elem) ? elem : max); // 9
+_.reduce([3,2,9,6], (count, elem) => count+1, 0);	 // 4
+
+
+
+// ソート 
+
+var objs = [{'name': 'Tanaka', 'age': 35},
+	    {'name': 'Yamada', 'age': 30},
+	    {'name': 'Satou',  'age': 41}];
+
+// age の値でソート
+_.sortBy(objs, 'age');
+//   { name: 'Yamada', age: 30 },
+//   { name: 'Tanaka', age: 35 },
+//   { name: 'Satou', age: 41 } ]
+
+// age で逆順ソート
+_.sortBy(objs, elem => elem.age * -1);
+// [ { name: 'Satou', age: 41 },
+//   { name: 'Tanaka', age: 35 },
+//   { name: 'Yamada', age: 30 } ]
+
+
+// 関数で指定
+var strs = ["foo", "bar", "BAZ", "qux"]
+_.sortBy(strs);	                           // [ 'BAZ', 'bar', 'foo', 'qux' ]
+_.sortBy(strs, elem => _.toLower(elem) );  // [ 'bar', 'BAZ', 'foo', 'qux' ]
+
+
+var points = [{'x': 2, 'y': 8},
+	      {'x': 5, 'y': 1},
+	      {'x': 2, 'y': 4}];
+
+// x でソートし、x が同じなら y でソート
+_.sortBy(points, ['x', 'y']);
+// [ { x: 2, y: 4 }, { x: 2, y: 8 }, { x: 5, y: 1 } ]
+
+
+
+
+// その他のメソッド
+
+result = _.size([3,2,9,6]);	// 4
+
+_.find([3,2,9,6], elem => (elem % 2) == 1);     // 3
+_.findLast([3,2,9,6], elem => (elem % 2) == 1); // 9
+
+_.includes([3,2,9,6], 2);	// true
+
+_.some([3,2,9,6], elem => (elem % 2) == 1);  // true
+_.every([3,2,9,6], elem => (elem % 2) == 1); // false
+
+
+// メソッド連結と遅延評価
+
+_.chain([3,2,9,6])		// _([3,2,9,6]) と同じ
+    .filter(elem => (elem % 2) == 1)
+    .map(elem => elem * 2)
+    .value();
+// [ 6, 18 ]
