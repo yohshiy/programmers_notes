bbd27b24ceadf9f81dc22262716adde1fa3cd7a6
yohshiy <yohshiy@gmail.com>
2016-02-29 04:20:36 +0900

    コマンド引数取得のサンプル追加

---
+++ node/hello_debug.js
@@ -0,0 +1,6 @@
+﻿
+console.log("Hello ");
+debugger;
+console.log("world!");
+
+
---
+++ node/process_arg/process_arg.js
@@ -0,0 +1,11 @@
+﻿'use strict'
+
+for (let cnt in process.argv) {
+    console.log('%d : %s', cnt, process.argv[cnt]);
+}
+
+console.log();
+console.log('process.execArgv = ', process.execArgv);
+console.log('process.execPath = ', process.execPath);
+console.log('__dirname        = ', __dirname);
+console.log('__filename       = ', __filename);
