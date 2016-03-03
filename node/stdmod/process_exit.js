39fb3e573fbbced2d592f7a78cf30576b175d172
yohshiy <yohshiy@gmail.com>
2016-03-03 19:01:05 +0900

    process.exit のサンプル

--- node/stdmod/process_exit.js
+++ node/stdmod/process_exit.js
@@ -1,10 +1,29 @@
 ﻿
-process.kill(process.pid);
+
+//// exit イベント
+////////////////////////////////////////////////////////////////
+
+process.on('exit',  (code)=> {
+	console.log('Program exit. code = ', code);
+});
+
+process.on('exit',  (code)=> {
+	// 非同期は実行されない
+	setImmediate(console.log, 'This will not run');
+});
+
+
+
+//// beforeExit イベント
+////////////////////////////////////////////////////////////////
 
 process.once('beforeExit', () => {
-	console.log("before exit");});
+	console.log("before exit");
+});
 
-process.on('exit', (code)=>console.log('Program exit code : ', code));
 
+//// プログラムの終了
+////////////////////////////////////////////////////////////////
 
 process.exit(1);
+
