779959c5c1e318629222e939c97bc9a64622d183
yohshiy <yohshiy@gmail.com>
2016-10-14 13:42:22 +0900

    二分探索コード

---
+++ lang/cpp/bsearch/binary_search.h
@@ -0,0 +1,56 @@
+﻿
+#ifndef __BINARY_SEARCH_H_LOADED__
+#define __BINARY_SEARCH_H_LOADED__
+
+#include <vector>
+
+
+template <typename ElementType>
+int binary_search(const std::vector<ElementType> &ary, ElementType key)
+{
+  const int KEY_NOT_FOUND = -1;
+  int imin = 0;
+  int imax = static_cast<int>(ary.size());
+
+  for (;imin <= imax;) {
+    int imid = imin + (imax - imin) / 2;
+    if (ary[imid] > key) {
+      imax = imid - 1;
+    } else if (ary[imid] < key) {
+      imin = imid + 1;
+    } else {
+      return imid;
+    }
+  }
+  return KEY_NOT_FOUND;
+}
+
+
+template <class RandomIterator, typename T>
+RandamIterator binary_search(RandamIterator first, RandamIterator last, const T &val)
+{
+  if (last <= first) {
+    return last;
+  }
+  size_t bgnidx = 0;
+  size_t endidx = last - first;
+
+  for (;bgnidx <= endidx;) {
+    size_t mididx = bgnidx + (endidx - bgnidx) / 2;
+    const T &midval = *(first+mididx);
+    if (midval < val) {
+      bgnidx = mididx + 1;
+    }
+    else if (midval == val) {
+      return first + mididx;
+    }
+    else {
+      endidx = mididx - 1;
+    }
+  }
+  return last;
+}
+
+
+#endif  // __BINARY_SEARCH_H_LOADED__
+
---
+++ lang/cpp/bsearch/test.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include "binary_search.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+
+static int ToInt(const char *str)
+{
+  stringstream ss;
+  ss << str;
+  int val;
+  ss >> val;
+  return val;
+}
+
+
+static void Usage()
+{
+  cout << "example_bsearch SEARCH_NUM, NUM1 NUM2 ..." << endl;
+}
+
+int main(int argc, char **argv)
+{
+  std::vector<int> vec;
+
+  if (argc < 3) {
+    Usage();
+    return 1;
+  }
+
+  int target = ToInt(argv[1]);
+  cout << "Array [";
+  for (int cnt = 2 ; cnt < argc ; cnt++) {
+    if (cnt != 2) {
+      cout << ", ";
+    }
+    vec.push_back(ToInt(argv[cnt]));
+    cout << vec.back();
+  }
+  cout << "]" << endl;
+
+  cout << "Find (" << target << ") => ";
+
+  std::vector<int>::const_iterator itr = binary_search(vec.begin(), vec.end(), target);
+  if (itr == vec.end()) {
+    cout << "(Not found)";
+  } else {
+    cout << *itr;
+  }
+  cout << endl;
+  
+  return 0;
+}
+
+  
