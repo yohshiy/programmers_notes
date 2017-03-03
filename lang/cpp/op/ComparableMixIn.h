276cdde9c71e5686e6648579df60c570b0a7d844
yohshiy <yohshiy@gmail.com>
2017-03-03 18:52:53 +0900

    Adds comparable mix-in

---
+++ lang/cpp/op/ComparableMixIn.h
@@ -0,0 +1,39 @@
+﻿/// @file
+/// Comparable Mix-in 
+///
+/// @author M. Yoshida<yohshiy@gmail.com>
+///
+/// MIT License
+///
+
+
+#ifndef __COMPARABLE_MIX_IN_H__
+#define __COMPARABLE_MIX_IN_H__
+
+/// 比較可能にする Mix-in
+template <class T>
+class ComparableMixIn
+{
+ public:
+  /// 比較用関数
+  ///
+  /// | 状態               |  戻り値  |
+  /// |--------------------|----------|
+  /// | other の方が大きい | 負の値   |
+  /// | other と一致       |    0     |
+  /// | other の方が小さい | 正の値   |
+  /// 
+  virtual int Compare(const T &other) const = 0;
+
+
+  // 演算子の実装
+  bool operator==(const T &other) const { return Compare(other) == 0; };
+  bool operator!=(const T &other) const { return Compare(other) != 0; };
+  bool operator< (const T &other) const { return Compare(other) <  0; };
+  bool operator<=(const T &other) const { return Compare(other) <= 0; };
+  bool operator> (const T &other) const { return Compare(other) >  0; };
+  bool operator>=(const T &other) const { return Compare(other) >= 0; };
+};
+
+#endif
+
---
+++ lang/cpp/op/use_mixin.cpp
@@ -0,0 +1,45 @@
+﻿/// @file
+/// ComparableMixIn のサンプル
+///
+///     $ g++ -std=c++11 -I. use_mixin.cpp
+/// 
+
+
+#include "ComparableMixIn.h"
+
+
+/// 点
+struct Point : public ComparableMixIn<Point>
+{
+  int x;
+  int y;
+
+  Point(int ax, int ay) :x(ax), y(ay) {}
+
+  /// 比較関数.
+  /// x で比較し、同じなら y で比較
+  virtual int Compare(const Point &other) const override
+  {
+    return ((x != other.x) ? (x - other.x) : (y - other.y));
+  }
+  
+};
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  Point a(1, 2), b(2, 3);
+
+  cout << boolalpha;
+  cout << "a == b : " << (a == b) << endl;
+  cout << "a != b : " << (a != b) << endl;
+  cout << "a <  b : " << (a <  b) << endl;
+  cout << "a <= b : " << (a <= b) << endl;
+  cout << "a >  b : " << (a >  b) << endl;
+  cout << "a >= b : " << (a >= b) << endl;
+}
+
