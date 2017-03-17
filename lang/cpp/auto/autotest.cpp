56f77cb1601a212aec57f8e53a4ae59402750486
yohshiy <yohshiy@gmail.com>
2017-03-17 17:51:20 +0900

    Add auto samples

--- lang/cpp/auto/autotest.cpp
+++ lang/cpp/auto/autotest.cpp
@@ -1,63 +1,79 @@
-﻿#include <iostream>
+﻿/// @file
+/// auto samples
+///
+///     $ g++ -std=c++14 autotest.cpp
+/// 
+
+#include <iostream>
+#include <stdint.h>
+#include <typeinfo>
+
 
 using namespace std;
 
+int foofunc()
+{
+  // ローカル変数
+  auto localver = 1;
+
+  // リテラルの工夫
+  auto cstr = "Hellow";  // const char *
+  auto str  = "Hellow"s; // std::string
+  cout << cstr << endl;
+  cout << str  << endl;
+
+  return localver;
+}
+
+
+/// グローバル変数
+extern int64_t ErrNo;
+
+// static 変数
+static auto stvar = 1.0;
 
 /// グローバル定数
 const auto VERION = "1.0.0";
 
 
-template <typename T>
-struct Point
+
+
+class Foo
 {
   /// メンバー変数
-  T x = 0;
-  T y = 0;
-
-  // auto x = 0; NG
+  int m_bar = 0;
+  // auto m_baz = 0; NG
 
   /// クラス変数
-  static const auto point_dim = 2;
-  // static auto point_dim = 2; NG const のみ OK
+  static const int Dim = 2;
+  // static int Dim = 2; NG const のみ OK
+  static constexpr double Mag = 0.5;
 
-  Point(T ax, T ay)
-      :x(ax), y(ay) {}
-
-  Point PlusAuto(auto val) const
-  {
-    return Point(static_cast<T>(x+val), static_cast<T>(y+val));
-  }
-
-  template <typename U>
-  auto Plus(U val) const
-  {
-    return Point<decltype(x+val)>(x+val, y+val);
-  }
-
-  // auto Plus(auto val) const
-  // {
-  //   return Point<decltype(x+val)>(x+val, y+val);
-  // }
-
+  /// クラス変数 (Auto)
+  static const auto DimAuto = 2;
+  static constexpr auto MagAuto = 0.5;
 };
 
 
-template <typename T, typename U>
-static auto Add(const Point<T> &a, U b)
+
+////////////////////////////////////////////////////////////////
+
+
+template <typename T>
+auto StdMax(const T &a, const T &b)
 {
-  return Point<decltype(a.x+b)>(a.x+b, a.y+b);
+  return (a < b) ? b : a;
 }
-
-
-template <typename T, typename U>
-static auto OldAdd(const Point<T> &a, U b) -> Point<decltype(a.x+b)>
-{
-  return {a.x+b, a.y+b};
-}
-
 
 template <typename T, typename U>
 auto MyMax(const T &a, const U &b)
+{
+  return (a < b) ? b : a;
+}
+
+
+template <typename T, typename U>
+auto MyMax2Return(const T &a, const U &b)
 {
   if (a < b) {
     return b;
@@ -69,35 +85,107 @@ auto MyMax(const T &a, const U &b)
 
 
 template <typename T, typename U>
-auto ExtMyMax(const T &a, const U &b) -> decltype(a+b)
+auto FixedMyMax2Return(const T &a, const U &b) -> decltype(a+b)
 {
-  return (a < b) ? b : a;
+  if (a < b) {
+    return b;
+  }
+  else {
+    return a;
+  }
+}
+
+////////////////////////////////////////////////////////////////
+
+template <typename T>
+struct Point
+{
+  T x;
+  T y;
+
+  Point(T ax, T ay)
+      :x(ax), y(ay) {}
+
+#if 0
+  // コンパイルは通らない
+  auto Plus(auto val) const
+  {
+    return Point(static_cast<T>(x+val), static_cast<T>(y+val));
+  }
+#endif
+
+  // 引数だけ auto
+  Point PlusAutoArg(auto val) const
+  {
+    return Point(static_cast<T>(x+val), static_cast<T>(y+val));
+  }
+
+  // 戻り値だけ auto
+  template <typename U>
+  auto PlusAutoReturn(U val) const
+  {
+    return Point<decltype(x+val)>(x+val, y+val);
+  }
+
+
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Point<T> &p)
+{
+  out << "Point(" << p.x << ", " << p.y << ")";
+  return out;
 }
 
 
-    
+#if 1
 
-// static 変数
-static auto stvar = 1.0;
+template <typename T, typename U>
+inline auto operator*(const Point<T> &a, U b)
+{
+  return Point<decltype(a.x*b)>(a.x*b, a.y*b);
+}
+
+#else
+
+template <typename T, typename U>
+inline auto operator*(const Point<T> &a, U b) -> Point<decltype(a.x*b)>
+{
+  return {a.x*b, a.y*b};
+}
+
+#endif
+
 
 
 int main()
-{
-  auto localver = 1;
+{      
+  cout << StdMax(4, 12) << endl;  // 12
 
-  auto pos = Point<int>(1, 2);
-  auto pluspos = pos.Plus(2.5);
-  auto pluspos2 = pos.PlusAuto(2.5);
+  cout << MyMax(4.2, 12) << endl;  // 12
+  cout << MyMax(4.2,  2) << endl;  // 4.2
+
+  cout << MyMax2Return(4, 12) << endl;  // 12
+  // cout << MyMax2Return(4.2, 12) << endl;  // コンパイルエラー  
+  cout << endl;
   
+  cout << FixedMyMax2Return(4.2, 12) << endl;  // 12.0
+  cout << FixedMyMax2Return(4.2,  2) << endl;  //  4.2
 
-  auto added = Add(pos, 0.5);  // Point<double>(1.5, 2.5)
+  cout << endl;
 
+  
+  cout << Point<int>(3, 2) * 0.5 << endl;  // Point<double>(1.5, 1.0)
+
+  Point<int> pos(3, 2);
+  cout << pos.PlusAutoArg(2.5) << endl;    // Point(5, 4)
+  cout << pos.PlusAutoReturn(2.5) << endl; // Point(5.5, 4.5)
+
+  
   auto plus = [](auto a, auto b) { return a + b; };
-  int result = plus(2, 3); // result = 5
-
-  cout << MyMax(10, 12) << endl;
-  cout << ExtMyMax(10.2, 12) << endl;
+  cout << plus(2,   3) << endl; // result = 5
+  cout << plus(2.5, 3) << endl; // result = 5.5
   
-  return localver;
+  return 0;
 }
 
---
+++ lang/cpp/cpp1114/auto.cpp
@@ -0,0 +1,88 @@
+﻿
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+
+extern double Mag;
+
+
+static auto mag = 1.0;
+
+
+const double MyPi = 3.14;
+
+const auto MyPiA = 3.14;
+
+
+class Foo
+{
+  static const int classval_int = 3;
+};
+
+  
+class Foo2
+{  
+  static const auto classval_int = 3;
+  static constexpr auto classval_dbl = 3.1;  
+};
+
+
+class Baz
+{
+  double objval_real = 3.15;
+  // auto objval_real_a = 3.15;  // NG  
+};
+
+
+// template <typename T>
+// auto MyMax(const T &a, const T &b)
+// {
+//   return (a < b) ? b : a;
+// }
+
+template <typename T, typename U>
+auto MyMax(const T &a, const U &b)
+{
+  return (a < b) ? b : a;
+}
+
+template <typename T, typename U>
+auto MyMax3(const T &a, const U &b)
+{
+  if (a < b) {
+    return b;
+  }
+  else {
+    return a;
+  }
+}
+
+
+
+
+
+int main()
+{
+  auto num = 5;                // int
+  auto cstr = "Hello";         // char *
+  auto str = string("Hello");  // string
+
+  const auto cnum = 10;
+  auto &ref = str;
+  
+  auto lstr = "Hello"s;  // string
+  
+
+  auto maxval = MyMax(3.1, 5);
+  auto maxval2 = MyMax(1, 2.3);
+  cout << maxval  << endl;
+  cout << maxval2 << endl;
+  auto maxval3 = MyMax3(1, 2.3);
+  cout << maxval3 << endl;
+  
+  
+  return 0;
+}
